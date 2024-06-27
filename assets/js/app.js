6785492438fca5b2be04f347796dbf7b9614e798
vikram parmar <vikram.parmar@cybercom.in>
2024-06-27 23:51:18 +0530

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,111 @@
-var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+const arr = [];
+let nameValue = '';
+let emailValue = '';
 
-const handleSubmit = (event) => {
-    const opt1 = document.getElementById('opt-1').value;
-    const opt2 = document.getElementById('opt-2').value;
-    const opt3 = document.getElementById('opt-3').value;
-    const opt4 = document.getElementById('opt-4').value;
-    const opt5 = document.getElementById('opt-5').value;
-    const opt6 = document.getElementById('opt-6').value;
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    const data = {
-        opt1,
-        opt2,
-        opt3,
-        opt4,
-        opt5,
-        opt6,
-        name,
-        email
-    };
-
-    console.log(data);
-
-    // send this data to mail address
- 
+const sendEmail = (from, name) => {
+    console.log('from, name: ', from, name);
     Email.send({
         Host: "smtp.gmail.com",
         Username: "vikramsinhparmar2812@gmail.com",
-        Password: "Vikram@28",
-        To: "vikramsinhparmar2812@gmail.com",   
-        From: "vikramsinhparmar2812@gmail.com",
-        Subject: "New message on contact form",
-        Body: `Name: ${name} <br/> Email: ${email} <br/> Option 1: ${opt1} <br/> Option 2: ${opt2} <br/> Option 3: ${opt3} <br/> Option 4: ${opt4} <br/> Option 5: ${opt5} <br/> Option 6: ${opt6}`,
-    }).then(
-        message => alert("mail sent successfully")
-    );
-
-
+        Password: "Vikrm@2812",
+        To: 'vikramsinhparmar2812@gmail.com',
+        From: from,
+        Subject: "Sending Email using javascript",
+        Body: `Well that was easy!! ${name}`,
+    })
+        .then(function (message) {
+            console.log('message: ', message);
+            alert("mail sent successfully")
+        }).
+        catch(function (error) {
+            alert(error)
+        });
 }
+
+const handleClick = (event) => {
+    var checkboxValue = event.target.value;
+    arr.push(checkboxValue);
+}
+
+const handleChangename = (event) => {
+    nameValue = event.target.value;
+}
+const handleChangeEmail = (event) => {
+    emailValue = event.target.value;
+}
+
+const handleSubmit = (event) => {
+
+    event.preventDefault();
+ 
+    if (arr.length === 0) {
+        alert("Please select at least one option");
+        return;
+    }
+    console.log(arr);
+    console.log(nameValue);
+    console.log(emailValue);
+
+    sendEmail(emailValue, nameValue);
+}
+
+const sliderData = [
+    {
+      href: "https://www.nilkanthtextilegroup.com",
+      imgSrc: "assets/img/work-victory.jpg",
+      imgAlt: "Victory",
+      title: "SHREE SHUKAN INDUSTRIES LLP",
+      description: "At Shree Shukan Textiles Industries we are all the time concerned about the way we create wealth"
+    },
+    {
+        href: "https://fantastic-food-customer-webapp.vercel.app/restaurant",
+        imgSrc: "assets/img/fflogo.png",
+        imgAlt: "Alex Nowak",
+        title: "Fantastic Food",
+        description: "Reservation, Digital Menu, Scan and Order, Restaurant management system"
+    },
+    {
+      href: "https://www.whisttler.com",
+      imgSrc: "assets/img/work-metiew-smith.jpg",
+      imgAlt: "Metiew and Smith",
+      title: "Whisttler",
+      description: "The Influencer Marketing Mercenaries."
+    },
+    {
+      href: "https://www.murlidharprinters.com/",
+      imgSrc: "assets/img/work-alex-nowak.jpg",
+      imgAlt: "Alex Nowak",
+      title: "Murlidharprinters",
+      description: "MPPL is powering high-fashion Indian wear brands spanning the globe, while delivering unparalleled excellence across the apparel value chain."
+    },
+    {
+      href: "https://play.google.com/store/apps/details?id=com.stiklasko.visitlasko",
+      imgSrc: "assets/img/lasko.png",
+      imgAlt: "Alex Nowak",
+      title: "Visit Lasko",
+      description: "An tourist application for lasko"
+    }
+  ];
+  
+  const slider = document.getElementById('slid');
+  
+  sliderData.forEach((item, index) => {
+    const li = document.createElement('li');
+    li.classList.add('slider--item');
+    
+    if(index === 0) li.classList.add('slider--item-left');
+    else if(index === 1) li.classList.add('slider--item-center');
+    else li.classList.add('slider--item-right');
+  
+    li.innerHTML = `
+      <a href="${item.href}">
+        <div class="slider--item-image">
+          <img src="${item.imgSrc}" alt="${item.imgAlt}">
+        </div>
+        <p class="slider--item-title">${item.title}</p>
+        <p class="slider--item-description">${item.description}</p>
+      </a>
+    `;
+    slider.appendChild(li);
+  });
+  
